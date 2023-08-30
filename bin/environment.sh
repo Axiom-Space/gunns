0c33571d3b684d8523918310e9bb9fd4d30d4f59
syam@axiomspace.com <syam@axiomspace.com>
2023-08-30 17:27:53 -0500

    Making grep better so it returns only failed tests, fixing something nasa didn't error on i guess, etc

--- bin/environment.sh
+++ bin/environment.sh
@@ -6,5 +6,5 @@ SWIG_PATCH_DIR="/projects/libs/scripts/convert_swig"
 export TRICK_HOME=$TRICK_DIR/trick
 export GUNNS_TRICK_HOME="${TRICK_HOME}"
 export GUNNS_HOME=$(dirname $(dirname $(readlink -f $BASH_SOURCE)))
-export GUNNS_EXT_PATH="${GUNNS_HOME}"
+export GUNNS_EXT_PATH=""
 source $GUNNS_HOME/bin/bashrc
